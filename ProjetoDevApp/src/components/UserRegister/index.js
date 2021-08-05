53a98a948dcf4561ffaab315360b300fd5108f7d
Bruna <bubu1azambuja@gmail.com>
2021-08-05 16:16:50 -0300

    First layout.

--- ProjetoDevApp/src/components/UserRegister/index.js
+++ ProjetoDevApp/src/components/UserRegister/index.js
@@ -1,14 +1,71 @@
 import React from 'react';
-import { SafeAreaView, ScrollView, StatusBar, Text } from 'react-native';
-import { Container } from './styles';
+import { TextInput } from 'react-native';
+import { Container, InfoText, InfoContainer, TitleText, InputsContainer } from './styles';
 
 const UserRegister = () => (
-  <SafeAreaView>
-    <StatusBar />
-    <ScrollView contentInsetAdjustmentBehavior="automatic">
-      <Container><Text>oie</Text></Container>
-    </ScrollView>
-  </SafeAreaView>
+  <Container>
+      <InfoContainer>
+        <InfoText>
+        As informações preenchidas serão divulgadas apenas para a pessoa com a qual
+        você realizar o processo de adoção e/ou apadrinhamento, após a formalização do processo.
+        </InfoText>
+      </InfoContainer>
+
+      <InputsContainer>
+        <TitleText>
+          INFORMAÇÕES PESSOAIS
+        </TitleText>
+        <TextInput
+          placeholderTextColor="#bdbdbd"
+          placeholder="Nome Completo"
+        />
+        <TextInput
+          placeholderTextColor="#bdbdbd"
+          placeholder="Idade"
+        />
+        <TextInput
+          placeholderTextColor="#bdbdbd"
+          placeholder="E-mail"
+        />
+        <TextInput
+          placeholderTextColor="#bdbdbd"
+          placeholder="Estado"
+        />
+        <TextInput
+          placeholderTextColor="#bdbdbd"
+          placeholder="Cidade"
+        />
+        <TextInput
+          placeholderTextColor="#bdbdbd"
+          placeholder="Endereço"
+        />
+        <TextInput
+          placeholderTextColor="#bdbdbd"
+          placeholder="Telefone"
+        />
+
+        <TitleText>
+          INFORMAÇÕES DE PERFIL
+        </TitleText>
+
+        <TextInput
+          placeholderTextColor="#bdbdbd"
+          placeholder="Nome do Usuário"
+        />
+        <TextInput
+          placeholderTextColor="#bdbdbd"
+          placeholder="Senha"
+        />
+        <TextInput
+          placeholderTextColor="#bdbdbd"
+          placeholder="Confirmação de senha"
+        />
+
+        <TitleText>
+          FOTO DE PERFIL
+        </TitleText>
+      </InputsContainer>
+  </Container>
 );
 
 export default UserRegister;
--- ProjetoDevApp/src/components/UserRegister/styles.js
+++ ProjetoDevApp/src/components/UserRegister/styles.js
@@ -1,5 +1,26 @@
 import styled from 'styled-components/native';
 
 export const Container = styled.View`
-  background-color: #335895;
+  padding: 16px;
+`;
+
+export const InfoText = styled.Text`
+  font-size: 14px;
+  text-align: center;
+  color: #434343;
+`;
+
+export const InfoContainer = styled.View`
+  background-color: #cfe9e5;
+  padding: 4px;
+  border-radius: 4px;
+`;
+
+export const TitleText = styled.Text`
+  padding: 30px 0 10px 0;
+  color: #88c9bf;
+`;
+
+export const InputsContainer = styled.View`
+  padding: 0 12px;
 `;
