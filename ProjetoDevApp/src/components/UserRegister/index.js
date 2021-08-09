5b6c4c6cf95adadf24cb110387c28862ad9877e7
Bruna <bubu1azambuja@gmail.com>
2021-08-09 16:20:48 -0300

    Final Layout.

--- ProjetoDevApp/src/components/UserRegister/index.js
+++ ProjetoDevApp/src/components/UserRegister/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
-import { TextInput } from 'react-native';
-import { Container, InfoText, InfoContainer, TitleText, InputsContainer } from './styles';
+
+import { Container, InfoText, InfoContainer, TitleText,
+  InputsContainer, Input, ButtonFile, ButtonText, ButtonView, Icon } from './styles';
+import Button from '../Button';
+
 
 const UserRegister = () => (
   <Container>
@@ -15,57 +18,39 @@ const UserRegister = () => (
         <TitleText>
           INFORMAÇÕES PESSOAIS
         </TitleText>
-        <TextInput
-          placeholderTextColor="#bdbdbd"
-          placeholder="Nome Completo"
-        />
-        <TextInput
-          placeholderTextColor="#bdbdbd"
-          placeholder="Idade"
-        />
-        <TextInput
-          placeholderTextColor="#bdbdbd"
-          placeholder="E-mail"
-        />
-        <TextInput
-          placeholderTextColor="#bdbdbd"
-          placeholder="Estado"
-        />
-        <TextInput
-          placeholderTextColor="#bdbdbd"
-          placeholder="Cidade"
-        />
-        <TextInput
-          placeholderTextColor="#bdbdbd"
-          placeholder="Endereço"
-        />
-        <TextInput
-          placeholderTextColor="#bdbdbd"
-          placeholder="Telefone"
-        />
+        <Input placeholderTextColor="#bdbdbd" placeholder="Nome Completo"/>
+        <Input placeholderTextColor="#bdbdbd" placeholder="Idade"/>
+        <Input placeholderTextColor="#bdbdbd" placeholder="E-mail"/>
+        <Input placeholderTextColor="#bdbdbd" placeholder="Estado"/>
+        <Input placeholderTextColor="#bdbdbd" placeholder="Cidade"/>
+        <Input placeholderTextColor="#bdbdbd" placeholder="Endereço"/>
+        <Input placeholderTextColor="#bdbdbd" placeholder="Telefone"/>
 
         <TitleText>
           INFORMAÇÕES DE PERFIL
         </TitleText>
-
-        <TextInput
-          placeholderTextColor="#bdbdbd"
-          placeholder="Nome do Usuário"
-        />
-        <TextInput
-          placeholderTextColor="#bdbdbd"
-          placeholder="Senha"
-        />
-        <TextInput
-          placeholderTextColor="#bdbdbd"
-          placeholder="Confirmação de senha"
-        />
+        <Input placeholderTextColor="#bdbdbd" placeholder="Nome do Usuário"/>
+        <Input placeholderTextColor="#bdbdbd" placeholder="Senha"/>
+        <Input placeholderTextColor="#bdbdbd" placeholder="Confirmação de senha"/>
 
         <TitleText>
           FOTO DE PERFIL
         </TitleText>
+          <ButtonFile >
+            <ButtonView>
+              {/* <Icon
+                name="plus-circle"
+                size={20}
+                color="#757575"
+              /> */}
+              <ButtonText>adicionar foto</ButtonText>
+            </ButtonView>
+          </ButtonFile>
+
+        <Button>FAZER CADASTRO</Button>
       </InputsContainer>
   </Container>
 );
+
 
 export default UserRegister;
--- ProjetoDevApp/src/components/UserRegister/styles.js
+++ ProjetoDevApp/src/components/UserRegister/styles.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components/native';
+import FeatherIcon from 'react-native-vector-icons/FontAwesome';
 
 export const Container = styled.View`
   padding: 16px;
@@ -22,5 +23,39 @@ export const TitleText = styled.Text`
 `;
 
 export const InputsContainer = styled.View`
-  padding: 0 12px;
+  padding: 24px 12px;
 `;
+
+export const Input = styled.TextInput`
+  border-bottom-color: #e6e7e8;
+  border-bottom-width: 1px;
+`;
+
+
+export const ButtonFile = styled.TouchableOpacity`
+  height: 128px;
+  width: 128px;
+  margin: 32px auto;
+  background-color: #e6e7e7;
+  elevation: 5;
+`;
+
+export const ButtonText = styled.Text`
+  color: #757575;
+  font-size: 14px;
+`;
+
+export const ButtonView = styled.View`
+  margin: auto;
+`;
+
+export const ButtonSubmit = styled.TouchableOpacity`
+  color: #434343;
+  background-color: #88c9bf;
+  margin-top: 32px;
+`;
+
+export const Icon = styled(FeatherIcon)`
+  margin-right: 16px;
+`;
+
