e498deed83baf990c3ca2dea3cc875e43c4ac683
gianlopes <54591501+gianlopes@users.noreply.github.com>
2021-08-24 16:25:39 -0300

    Fix for router logic (login necessary to register)

--- ProjetoDevApp/src/components/Router/styles.js
+++ ProjetoDevApp/src/components/Router/styles.js
@@ -1,23 +1,26 @@
 import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
-  loginButton: {
+  logoutButton: {
     backgroundColor: '#88c9bf',
     alignSelf: 'center',
     marginTop: 20,
-  },
-  loginButtonText: {
-    color: '#434343',
-    fontSize: 16,
-  },
-  loginButtonContent: {
-    textAlignVertical: 'center',
     height: 54,
     width: 280,
+    elevation: 5,
+  },
+  buttonText: {
+    color: '#434343',
+    fontSize: 16,
+    textTransform: 'uppercase',
+    fontWeight: 'normal',
   },
   animalRegisterButton: {
     backgroundColor: '#ffd358',
     alignSelf: 'center',
     marginTop: 20,
+    height: 54,
+    width: 280,
+    elevation: 5,
   }
 });
--- ProjetoDevApp/src/routes/index.js
+++ ProjetoDevApp/src/routes/index.js
@@ -1,15 +1,16 @@
 import React, { useContext } from 'react';
-import { View, ActivityIndicator } from 'react-native';
 
 import UserRegister from '../components/UserRegister';
 import Cadastro_Animais from '../components/AnimalRegister/cadastro_animais';
 import TelaFinalCadastroAnimais from '../components/AnimalRegister/CadastroAnimaisFinalizado';
 import UserLogin from '../components/UserLogin';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import LoginOrRegister from '../components/LoginOrRegister';
 import Router from '../components/Router';
 
 import { LoginContext } from '../utils/LoginProvider';
 import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
 
 const App = createNativeStackNavigator();
 
@@ -25,36 +26,52 @@ const Routes = () => {
           },
         }}>
         {user ? (
-          <App.Screen name="Home" component={Router} />
+          <>
+            <App.Screen name="Home" component={Router} />
+            <App.Screen
+              name="AnimalRegister"
+              options={{
+                headerStyle: { backgroundColor: '#fee29b' },
+                title: 'Cadastro de Animais',
+              }}
+              component={Cadastro_Animais}
+            />
+            <App.Screen
+              name="AnimalRegister2"
+              options={{
+                headerStyle: { backgroundColor: '#fee29b' },
+                title: 'Cadastro de Animais',
+              }}
+              component={TelaFinalCadastroAnimais}
+            />
+          </>
         ) : (
-          <App.Screen
-            name="Login"
-            options={{ headerStyle: { backgroundColor: '#cfe9e5' } }}
-            component={UserLogin}
-          />
+          <>
+            <App.Screen
+              name="LoginOrRegister"
+              options={{
+                headerStyle: { backgroundColor: '#88c9bf' },
+                title: 'Cadastro',
+                headerTitleStyle: { color: '#434343' },
+              }}
+              component={LoginOrRegister}
+            />
+            <App.Screen
+              name="UserLogin"
+              options={{ headerStyle: { backgroundColor: '#cfe9e5' } }}
+              component={UserLogin}
+            />
+            <App.Screen
+              name="UserRegister"
+              options={{
+                headerStyle: { backgroundColor: '#cfe9e5' },
+                title: 'Cadastro',
+                headerTitleStyle: { color: '#434343' },
+              }}
+              component={UserRegister}
+            />
+          </>
         )}
-
-        <App.Screen
-          name="UserRegister"
-          options={{ headerStyle: { backgroundColor: '#cfe9e5' } }}
-          component={UserRegister}
-        />
-        <App.Screen
-          name="AnimalRegister"
-          options={{
-            headerStyle: { backgroundColor: '#fee29b' },
-            title: 'Cadastro de Animais',
-          }}
-          component={Cadastro_Animais}
-        />
-        <App.Screen
-          name="AnimalRegister2"
-          options={{
-            headerStyle: { backgroundColor: '#fee29b' },
-            title: 'Cadastro de Animais',
-          }}
-          component={TelaFinalCadastroAnimais}
-        />
       </App.Navigator>
     </NavigationContainer>
   );
