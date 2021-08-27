68f0e11ceb8d4fbd3b98a0937f8aec3a6f8cc75b
gianlopes <54591501+gianlopes@users.noreply.github.com>
2021-08-27 20:53:53 -0300

    Add eslint settings

--- ProjetoDevApp/.eslintrc.js
+++ ProjetoDevApp/.eslintrc.js
@@ -2,6 +2,10 @@ module.exports = {
   root: true,
   extends: '@react-native-community',
   rules: {
-    "no-unused-vars": "error",
-  }
+    'no-unused-vars': 'error',
+    'global-require': 0,
+    'eslint linebreak-style': [0, 'error', 'windows'],
+    'prettier/prettier': ['error', { endOfLine: 'auto' }],
+    indent: ['off', 2],
+  },
 };
