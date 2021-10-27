6e8dbd9d39d3b3d43a8ace71778377e8878664a8
gianlopes <54591501+gianlopes@users.noreply.github.com>
2021-10-26 23:57:12 -0300

    Add sidebar menu

--- ProjetoDevApp/App.js
+++ ProjetoDevApp/App.js
@@ -1,6 +1,9 @@
+import 'react-native-gesture-handler';
 import React from 'react';
 import Routes from './src/routes';
 import LoginProvider from './src/utils/LoginProvider';
+import {LogBox } from 'react-native';
+LogBox.ignoreLogs(['Reanimated 2']);
 
 const App = () => {
   return (
